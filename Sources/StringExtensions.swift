a24e2b21c66a8e4ab0aadfdde45cc1face56e707
Jonathan Guthrie <jono@perfect.org>
2016-10-25 18:59:30 -0400

    adding base64 functions

--- Sources/StringExtensions.swift
+++ Sources/StringExtensions.swift
@@ -374,4 +374,16 @@ public extension String {
 		#endif
 		return regex?.firstMatch(in: self, options: [], range: NSMakeRange(0, self.characters.count)) != nil
 	}
+
+	/// Encode a String to Base64
+	func toBase64() -> String {
+		return Data(self.utf8).base64EncodedString()
+	}
+
+	/// Decode a String from Base64. Returns nil if unsuccessful.
+	func fromBase64() -> String? {
+		guard let data = Data(base64Encoded: self) else { return nil }
+		return String(data: data, encoding: .utf8)
+	}
 }
+
