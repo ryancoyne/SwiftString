0601aa8e5c1bbf6d6a47c1241eefaade79b6d6a9
Ryan <ryan@buckettechnologies.com>
2018-12-23 14:24:41 -0500

    Reinstated the gitignore file and updating the package file

--- Package.swift
+++ Package.swift
@@ -1,9 +1,18 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-11-11 21:12:58 +0000
+// swift-tools-version:4.0
 import PackageDescription
 let package = Package(
 	name: "SwiftString",
-	targets: [],
-	dependencies: [
+	products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+            .library(
+                name: "SwiftString",
+                targets: ["SwiftString"]),
+        ],
+	dependencies: [],
+	targets: [
+	    .target(
+	        name: "SwiftString",
+    	 	dependencies: [],
+		path: "Sources"),
 	]
 )
